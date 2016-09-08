a15a52fcd263205477ed6f7c0ecec1f355c8ff3d
root <root@5.1.89.239>
2016-09-08 13:28:34 -0400

    <Folder>

---
+++ app/views/layouts/_news.php
@@ -0,0 +1,8 @@
+<?php if (!CONFIG()->enable_news_ticker) return; ?>
+<div id="news-ticker" style="display: none">
+  <ul>
+    <li>Nothing to see here. :^)</a></li>
+  </ul>
+
+  <a href="#" id="close-news-ticker-link"><?= $this->t('.close') ?></a>
+</div>
